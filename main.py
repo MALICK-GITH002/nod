c87e2266e1eb79862fa3c7cfa72468412f8940a9
MALICK-GITH002 <nightaddes@gmail.com>
2025-05-13 07:02:25 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,13 +1,15 @@
 from telegram import Update
 from telegram.ext import Application, CommandHandler, CallbackContext
 
-def start(update: Update, context: CallbackContext) -> None:
-    update.message.reply_text("Bonjour ! Utilisez la commande /scan pour scanner un réseau.\n"
-                              "Exemple : /scan 192.168.1 1 10 80")
+async def start(update: Update, context: CallbackContext) -> None:
+    await update.message.reply_text(
+        "Bonjour ! Utilisez la commande /scan pour scanner un réseau.\n"
+        "Exemple : /scan 192.168.1 1 10 80"
+    )
 
 def main():
     # Remplacez 'YOUR_TOKEN_HERE' par votre token Telegram
-    application = Application.builder().token("8036490999:AAFlp8YuCAhjAd6wQumtUtdSs9HUMWKahFo").build()
+    application = Application.builder().token("YOUR_TOKEN_HERE").build()
 
     # Ajouter le gestionnaire pour /start
     application.add_handler(CommandHandler("start", start))
